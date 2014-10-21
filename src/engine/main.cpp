512d025c2c0993173015838d612a1ceed7fdb1fb
a-teammate <madoe3@web.de>
2014-10-21 23:42:03 +0200

    make paths relative to the executing file. texturedir became obsolete
    so far: texture-paths and font texture-paths are relative to the file they get executed from.
    
    bug (but probably not to be fixed): commandline texture-adding wont work

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -1216,7 +1216,7 @@ int main(int argc, char **argv)
     logoutf("init: gl");
     gl_checkextensions();
     gl_init(useddepthbits, usedfsaa);
-    notexture = textureload(tempformatstring("%s/notexture.png", texturedir));
+    notexture = textureload("media/texture/notexture.png");
     if(!notexture) fatal("could not find core textures");
 
     logoutf("init: console");
